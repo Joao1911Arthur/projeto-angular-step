be6cb24b183272029479f095c0985a80475b0887
Joao1911Arthur <arthur1911br@gmail.com>
2026-04-28 18:47:22 -0300

    aula 3 data binding e pipes

--- minha-aplicacao-angular/src/app/app.ts
+++ minha-aplicacao-angular/src/app/app.ts
@@ -1,10 +1,12 @@
 
 import { Component, signal } from '@angular/core';
 import { Aula3 } from './aula3/aula3';
+import { PainelAdm } from './painel-adm/painel-adm';
+
 
 @Component({
   selector: 'app-root',
-  imports: [Aula3],
+  imports: [Aula3, PainelAdm],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- minha-aplicacao-angular/src/app/painel-adm/painel-adm.ts
+++ minha-aplicacao-angular/src/app/painel-adm/painel-adm.ts
@@ -1,23 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 interface Pessoas {
   nome: string;
   id: number;
+  salario:number;
 }
 
 @Component({
   selector: 'app-painel-adm',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './painel-adm.html',
   styleUrls: ['./painel-adm.css'],
 })
 
 export class PainelAdm {
   users: Pessoas[] = [
-    { nome: 'João', id: 1 },
-    { nome: 'Maria', id: 2 },
-    { nome: 'Pedro', id: 3 },
-    { nome: 'Ana', id: 4 },
-    { nome: 'Carlos', id: 5 },
+    { nome: 'João', id: 1, salario: 10000 },
+    { nome: 'Maria', id: 2, salario: 5000 },
+    { nome: 'Pedro', id: 3, salario: 3000 },
+    { nome: 'Ana', id: 4, salario: 7000 },
+    { nome: 'Carlos', id: 5, salario: 2000 },
   ];
 }
